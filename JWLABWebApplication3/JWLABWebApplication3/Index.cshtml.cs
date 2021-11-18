c8d6dd217df8f4359e7535f95faf4cd0aa06ec1a
juleszee <94473542+juleszee@users.noreply.github.com>
2021-11-18 14:53:34 +0000

    Add files via upload

---
+++ JWLABWebApplication3/JWLABWebApplication3/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JWLABWebApplication3
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
